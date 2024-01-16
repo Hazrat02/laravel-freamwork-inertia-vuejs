8d65d4f030e84197629270b579f17f27492052d3
Hazrat02 <hazratbd80@gmail.com>
2024-01-16 08:16:43 -0800

    ssr add

---
+++ resources/js/Components/CustomLoading.vue
@@ -0,0 +1,22 @@
+<template>
+    <!-- Your custom loading indicator markup and styles go here -->
+    <div class="custom-loading-indicator">
+        Loading...
+    </div>
+</template>
+
+<script>
+export default {
+    // Your component options go here
+}
+</script>
+
+<style scoped>
+/* Your custom loading indicator styles go here */
+.custom-loading-indicator {
+    /* Add your custom styles for the loading indicator */
+    color: #ff9900;
+    font-size: 1.5rem;
+    /* ... add more styles as needed */
+}
+</style>
